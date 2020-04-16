3a67270bf3b5cfe17ca82952559fe304ad2e52cd
Paul-Larkin <realpaullarkin@gmail.com>
2020-04-16 17:22:39 +0100

    Commit_finnished_Assignment1

---
+++ OCP_Java_SE_8_1Z0-809_Assignments/src/assignment1/Assignment1_5.java
@@ -0,0 +1,30 @@
+package assignment1;
+
+public class Assignment1_5 {
+
+	public static void main(String[] args) {
+		
+		ShoeSize small = ShoeSize.SMALL;
+		ShoeSize medium = ShoeSize.MEDIUM;
+		ShoeSize large = ShoeSize.valueOf("LARGE");
+		
+		System.out.println("My size: " + small);
+		System.out.println("Your size: " + medium);
+		System.out.println("Big size: " + large);
+	}// main
+}// class
+
+enum ShoeSize{
+	
+	SMALL(8), MEDIUM(10), LARGE(16);
+	
+	private int size;
+
+	ShoeSize(int size) {
+		this.size = size;
+	}// constructor
+	
+	public int getSize() {
+		return this.size;
+	}// method
+}// enum
