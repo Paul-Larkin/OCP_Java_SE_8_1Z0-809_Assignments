5008024a46f40d1371ee8ffe5f3bbf4844275a86
Paul-Larkin <realpaullarkin@gmail.com>
2020-04-16 14:16:45 +0100

    Commit_Assignment1_2, fixed Assignment_1_1

--- OCP_Java_SE_8_1Z0-809_Assignments/src/assignment1/Assignment1_1.java
+++ OCP_Java_SE_8_1Z0-809_Assignments/src/assignment1/Assignment1_1.java
@@ -7,8 +7,8 @@ public class Assignment1_1 {
 			float poppingSpeed = 2.0f;
 			System.out.println("Popcorn is popping at a speed of: " + poppingSpeed);
 			System.out.println("Popcorn is now ready");
-		}
-	}//inner class
+		}// method
+	}// inner class
 	
 	public static void main(String[] args) {
 		/* Two line approach
@@ -16,8 +16,8 @@ public class Assignment1_1 {
 		PopcornMaker p = a.new PopcornMaker();
 		*/
 		
-		//One line approach
+		// One line approach
 		PopcornMaker p = new Assignment1_1().new PopcornMaker();
 		p.makePopCorn();
-	}//main
-}//class
+	}// main
+}// class
