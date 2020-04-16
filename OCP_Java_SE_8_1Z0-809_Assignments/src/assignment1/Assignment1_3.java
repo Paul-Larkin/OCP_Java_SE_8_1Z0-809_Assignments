a61e1221372c917ce3970f80f2ea22c5196bb8de
Paul-Larkin <realpaullarkin@gmail.com>
2020-04-16 15:18:46 +0100

    Commit as far as Assignment1_4B

---
+++ OCP_Java_SE_8_1Z0-809_Assignments/src/assignment1/Assignment1_3.java
@@ -0,0 +1,31 @@
+package assignment1;
+
+public class Assignment1_3 {
+	
+	public void makeMeal() {
+		Blender blender = new Blender() {
+			@Override
+			public void blend() {
+				System.out.println("Dinner being prepared...liquidizing food.");
+			}// method
+		};// anon class
+		blender.blend();
+	}// method
+
+	public static void main(String[] args) {
+		Assignment1_3 obj = new Assignment1_3();
+		obj.makeMeal();
+	}// main
+} //class
+
+interface Blender {
+	public void blend();
+}// interface
+
+/*
+class Blender {
+	public void blend() {
+		System.out.println("Generic Blending");
+	}// method
+}// class
+*/
