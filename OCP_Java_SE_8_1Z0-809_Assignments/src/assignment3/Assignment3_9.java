ca4065384c2dfd92b1266745e9ff09399c29ba9f
Paul-Larkin <realpaullarkin@gmail.com>
2020-06-06 19:35:14 +0100

    Last few Generics Assignments

---
+++ OCP_Java_SE_8_1Z0-809_Assignments/src/assignment3/Assignment3_9.java
@@ -0,0 +1,26 @@
+package assignment3;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Assignment3_9 {
+
+	public static void main(String[] args) {
+		
+		Map<String, String> brokers = new HashMap<>();
+		brokers.put("Best Insurance Ltd", "Dann Walsh");
+		brokers.put("Top Insurance Ltd", "Dann Walsh");
+		brokers.put("Permier Insurance Ltd", "Jesse Nolan");
+		brokers.put("Premium Insurance Ltd", "Linda Martin");
+		
+		// A: Display Contents
+		brokers.forEach((key,value) -> System.out.println("Key: " +key + " Value: " + value));	
+		
+		// B: Update Dann Walsh
+		brokers.forEach((key, value) -> brokers.replace(key, "Dann Walsh", "Linda Martin"));
+		
+		// C: Display Updated Contents
+		brokers.forEach((key, value) -> System.out.println("Key: " +key + " Value: " + value));
+	}
+
+}// class
